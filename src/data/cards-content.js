dd639b058b094db92b68fc913ac5e81ca0618273
Hector Rodriguez <hector@hrod.dev>
2023-10-27 21:56:52 -0500

    First commit

---
+++ src/data/cards-content.js
@@ -0,0 +1,20 @@
+[
+    {
+        "icon": "/images/icon-sedans.svg",
+        "alt": "Sedans Icon",
+        "title": "Sedans",
+        "description": "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+    },
+    {
+        "icon": "/images/icon00-suvs.svg",
+        "alt": "Subs Icon",
+        "title": "SUVs",
+        "description": "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+    },
+    {
+        "icon": "/images/icon-luxury.svg",
+        "alt": "Luxury Icon",
+        "title": "Luxury",
+        "description": "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
+    }
+]
